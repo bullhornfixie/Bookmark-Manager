898a929a43e5d5dc6e9d2605cf8a968af6a667ff
Anna Cavalla <anna.cavalla@gmail.com>
2021-01-26 15:08:49 +0000

    added bookmark class

--- app.rb
+++ app.rb
@@ -3,9 +3,13 @@ require 'sinatra/base'
 class BookmarkManager < Sinatra::Base
 
   get '/' do
-    'Bookmark Manager'
-  end 
+    erb :index
+  end
+
+  get '/bookmarks' do
+    @bookmarks = Bookmark.all
+    erb :bookmarks
+  end
 
   run! if app_file == $0
-end 
-
+end
